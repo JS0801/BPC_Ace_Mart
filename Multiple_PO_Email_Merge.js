4772cb72d591e6c9fca43d9ddb7efdef74cbbc53
JS0801 <jsuthar@oncloudconsulting.ca>
2026-07-16 10:45:50 -0400

    Update from NetSuite (Auto)

--- Multiple_PO_Email_Merge.js
+++ Multiple_PO_Email_Merge.js
@@ -25,7 +25,7 @@ define([
 ) {
 
     var TEMP_FOLDER_ID = 8768;
-    var PARAM_HTML_FILE_ID = 'custscript_po_email_html_file';.
+    var PARAM_HTML_FILE_ID = 'custscript_po_email_html_file';
     var GROUP_PDF_TEMPLATE_ID = 'custtmpl_custom_grouped_pos_pdfhtml_template';
 
     var FIELD_EMAIL_SENT = 'custbody_email_sent';
